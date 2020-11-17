d3762e8849d0f3394a5e90e0351d10f14bcf4e1f
Z <nyitraizsolt1993>
2020-11-17 21:02:24 +0200

    fetch url by author or title or both

---
+++ src/components/BookItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BookItem({title}) {
+    return (
+        <div>
+            {title}
+        </div>
+    )
+}
+
+export default BookItem
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,28 +1,60 @@
 import React,{useState,useEffect} from 'react'
+import BookItem from './BookItem'
 
 function Books() {
     const API='AIzaSyCCtlMIvk5tLvK8mCWcRe4RaWn8MbsiOro'
-    const [title,setTitle]=useState('')
-    const [author,setAuthor]=useState('')
-    const [search,setSearch]=useState('')
+    const [books,setBooks]=useState([])
+    const [titleSearch,setTitleSearch]=useState('')
+    const [authorSearch,setAuthorSearch]=useState('')
 
 
-    let AutorURL=`https://www.googleapis.com/books/v1/volumes?q=intitle:${title}+inauthor:${author}&key=${API}`
 
-    const onSearchChange=(e)=>{
-        
-        console.log(search)
-    }
+    const onSearchChange= async(e)=>{
+        //IF I SEARCH FOR AUTHOR AND TITLE
+        if (titleSearch.length>0&&authorSearch.length>0){
+            URL=`https://www.googleapis.com/books/v1/volumes?q=intitle:${titleSearch}+inauthor:${authorSearch}&key=${API}`
+        }
+        //IF I SEARCH FOR AUTHOR
+        else if(titleSearch.length===0&&authorSearch.length>0){
+            URL=`https://www.googleapis.com/books/v1/volumes?q=inauthor:${authorSearch}&key=${API}`
+        }
+        //IF I SEARCH FOR TITLE
+        else{
+            URL=`https://www.googleapis.com/books/v1/volumes?q=intitle:${titleSearch}&key=${API}`
+        }
+        console.log(URL)
+        await fetch(URL)
+            .then((response)=>response.json())
+            .then(data=>{
+                console.log(data.items)
+                setBooks(data.items)
+            })
+        }
+
 
     return (
         <div>
-            <input 
+            <input
+                className='author-search' 
                 placeholder={'author'}
                 type="search" 
-                onChange={(e)=>setSearch(e.target.value)} 
-                value={search}
+                onChange={(e)=>setAuthorSearch(e.target.value)} 
+                value={authorSearch}
+            />
+            <input
+                className='title-search' 
+                placeholder={'title'}
+                type="search" 
+                onChange={(e)=>setTitleSearch(e.target.value)} 
+                value={titleSearch}
             />
             <button onClick={onSearchChange}>sd</button>
+            {books.map(book=>(
+                    <BookItem
+                        key={book.id}
+                        title={book.volumeInfo.title}
+                    />
+            ))}
         </div>
     )
 }
