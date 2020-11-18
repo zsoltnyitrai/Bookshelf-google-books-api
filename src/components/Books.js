8c4ed7cb091bbac8ce9fd4fb8c14c2067a68f9b5
Z <nyitraizsolt1993>
2020-11-18 16:26:29 +0200

    git commit doesn not work

--- src/components/Books.js
+++ src/components/Books.js
@@ -10,7 +10,7 @@ function Books() {
 
 
     const onSearchChange= async(e)=>{
-        //IF I SEARCH FOR AUTHOR AND TITLE
+        //IF I SEARCH FOR AUTHOR AND TITLEe
         if (titleSearch.length>0&&authorSearch.length>0){
             URL=`https://www.googleapis.com/books/v1/volumes?q=intitle:${titleSearch}+inauthor:${authorSearch}&key=${API}`
         }
